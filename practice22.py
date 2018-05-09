71a14d98e7b888d6e64c90bb14caca9c066173ae
SeoJeongHyun <30520300+rhaxlwo21@users.noreply.github.com>
2018-05-09 18:00:56 +0900

    Create practice22.py

---
+++ practice22.py
@@ -0,0 +1,12 @@
+num = int(input("원하는 단은:"))
+
+i=1
+#while i<=9:
+#    print(num,"X",i,"=",num*i)
+#    i+=1
+#print("구구단 출력 끝")
+
+for i in range(1,10,1):
+    print(num,"X",i,"=",num*i)
+
+print("구구단 출력 끝")
