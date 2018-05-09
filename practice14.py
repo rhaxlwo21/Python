acda2fe7ff57e85d4f92171fd4a34920815db465
SeoJeongHyun <30520300+rhaxlwo21@users.noreply.github.com>
2018-05-09 17:57:39 +0900

    Create practice14.py

---
+++ practice14.py
@@ -0,0 +1,16 @@
+#반복문으로 도형그리기
+
+#import turtle
+#t = turtle.Turtle()
+
+#for count in range(6):
+#    t.circle(100)
+#    t.left(360/6)
+
+######################################3
+import turtle
+t = turtle.Turtle()
+
+for count in range(360):
+    t.forward(1)
+    t.left(1)
