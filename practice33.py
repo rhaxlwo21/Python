79a199949126082d399498b94bddd476c8834c1e
SeoJeongHyun <30520300+rhaxlwo21@users.noreply.github.com>
2018-05-09 18:04:29 +0900

    Create practice33.py

---
+++ practice33.py
@@ -0,0 +1,16 @@
+heroes = ['아이언맨','토르','헐크','스칼렛 위치']
+
+print(heroes)
+
+heroes.sort()
+print(heroes)
+
+#heroes.sort()      정렬
+#print(heroes)
+
+#for hero in heroes:    heroes에 있는 정보를 hero에 복사
+#    print(hero)
+
+heroes.reverse()
+
+print(heroes)
