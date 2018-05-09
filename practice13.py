6045e61cc2335b3af7ef08e8755b779c7b0a0ad7
SeoJeongHyun <30520300+rhaxlwo21@users.noreply.github.com>
2018-05-09 17:57:15 +0900

    Create practice13.py

---
+++ practice13.py
@@ -0,0 +1,17 @@
+#반복문 연습하기
+
+#for i in [1,2,3,4,5]:
+#    print("방문을 환영합니다")
+
+#for i in [1,2,3,4,5]:
+#    print("방문을 환영합니다.")
+#    print("i 값은",i)
+
+#for i in [1,2,3,4,5,6,7,8,9]:
+#    print("9*",i,"=",9*i)
+
+#for i in range(1,6,1):
+#    print("방문을환영합니다.",i)
+for i in range(2,13,2):
+    print(i)
+
