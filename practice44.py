22b141b0f2a2334f59822f5af1d5f429f43796f3
SeoJeongHyun <30520300+rhaxlwo21@users.noreply.github.com>
2018-05-23 19:57:53 +0900

    Create practice44.py

---
+++ practice44.py
@@ -0,0 +1,8 @@
+#파일 복사와 삭제 연
+
+import shutil
+import os
+
+#shutil.copy('d:\\phones1.txt','d:\\phones2.txt')   #복사
+#shutil.move('d:\\phones2.txt','d:\\phones3.txt')    #원본삭제 후 복사
+os.remove('d:\\phones3.txt')
