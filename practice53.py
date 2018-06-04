cd1a01319d3d78ec19b03e45ae96752abf1c51c8
SeoJeongHyun <30520300+rhaxlwo21@users.noreply.github.com>
2018-06-04 18:30:57 +0900

    Create practice53.py

---
+++ practice53.py
@@ -0,0 +1,19 @@
+#class, __str__, self
+class Car:
+    def __init__(self, speed, color, model):
+        self.speed = speed
+        self.color = color
+        self.model = model
+
+    def drive(self):
+        self.speed = 60
+
+    def __str__(self):
+        msg = "속도:"+str(self.speed)+"/색상:"+str(self.color)+"/모델:"+str(self.model)
+        return msg
+
+dadCar = Car(0,"silver","A6")
+momCar = Car(0,"white","520d")
+myCar = Car(0,"blue","E-class")
+
+print(dadCar)
